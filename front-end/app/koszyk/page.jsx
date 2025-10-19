c1d6b835abd28f560e3cfdc5021197db7489d87e
Paweł Brzozowski <pawel@brzoza.net>
2025-10-19 13:46:30 +0200

    Refactor Cart component to utilize new structure; replace old CartProductsList and FlexContainer components with updated implementations, enhancing the shopping cart functionality and layout.

--- front-end/app/koszyk/page.jsx
+++ front-end/app/koszyk/page.jsx
@@ -1,7 +1,28 @@
+"use client";
+import { useContext } from "react";
+import { CenteredContent } from "../components/CenteredContent/CenteredContent";
+import { PageTitle } from "../components/PageTitle/PageTitle";
+import { CartContext } from "../contexts/CartContext";
+import { FlexContainer } from "../components/FlexContainer/FlexContainer";
+import { CartProductsList } from "../components/CartProductsList/CartProductsList";
+
 export default function Cart() {
+  const [shoppingCart] = useContext(CartContext);
+
   return (
     <>
-      <p>Koooszyk</p>
+      <CenteredContent>
+        {shoppingCart.length ? (
+          <PageTitle>Koszyk {`(${shoppingCart.length})`}</PageTitle>
+        ) : (
+          <PageTitle>Twój koszyk jest pusty</PageTitle>
+        )}
+
+        <FlexContainer>
+          <CartProductsList />
+          <CartSummary />
+        </FlexContainer>
+      </CenteredContent>
     </>
   );
 }
