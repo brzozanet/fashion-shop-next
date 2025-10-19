64a0b3481381c917d9c081f103b5d6197d887237
Paweł Brzozowski <pawel@brzoza.net>
2025-10-19 16:45:13 +0200

    Add BACKEND_URL to .env.example and update Favourites component to fetch data from backend

--- front-end/app/ulubione/page.jsx
+++ front-end/app/ulubione/page.jsx
@@ -1,7 +1,28 @@
-export default function Favourites() {
+import { CenteredContent } from "../components/CenteredContent/CenteredContent";
+import { PageTitle } from "../components/PageTitle/PageTitle";
+
+export default async function Favourites() {
+  const BACKEND_URL = process.env.BACKEND_URL;
+
+  const [favourites, products] = await Promise.all([
+    (await fetch(`${BACKEND_URL}/favourites`)).json(),
+    (await fetch(`${BACKEND_URL}/products`)).json(),
+  ]);
+
   return (
     <>
-      <p>Uluuubione</p>
+      <CenteredContent>
+        {userFavouritesProducts.length ? (
+          <>
+            <PageTitle>
+              Ulubione {`(${userFavouritesProducts.length})`}
+            </PageTitle>
+            <FavouritesList data={data} />
+          </>
+        ) : (
+          <PageTitle>Nie masz żadnych ulubionych produktów</PageTitle>
+        )}
+      </CenteredContent>
     </>
   );
 }
