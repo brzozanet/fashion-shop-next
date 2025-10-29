8b17fc1af469f8e1db450f10fd3873e29afa036f
Paweł Brzozowski <pawel@brzoza.net>
2025-10-29 13:47:25 +0100

    Refactor GenderPage component to utilize GENDERS_MAPPING for dynamic gender handling, update data fetching logic, and comment out unused Bestsellers component.

--- front-end/app/[gender]/page.jsx
+++ front-end/app/[gender]/page.jsx
@@ -1,14 +1,23 @@
 import { Bestsellers } from "../components/Bestsellers/Bestsellers";
 import { Hero } from "../components/Hero/Hero";
+import { GENDERS_MAPPING } from "../constants/mappings";
 
-export default function GenderPage() {
+export default async function GenderPage({ params }) {
+  const gender = GENDERS_MAPPING.get((await params).gender);
+
+  const genderResponse = await fetch(`http://localhost:3000/${gender}`);
+  const genderProducts = await genderResponse.json();
+
+  console.log(genderResponse);
+  console.log(genderProducts);
+
   return (
     <>
       <Hero imageUrl={genderProducts.heroImageUrl} />
-      <Bestsellers
+      {/* <Bestsellers
         bestsellersData={genderProducts.bestsellers}
         favouritesData={favouriteProducts}
-      />
+      /> */}
     </>
   );
 }
