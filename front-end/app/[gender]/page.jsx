467c808601c05c7b1f9005c0c3c98edf9c4c58ca
Paweł Brzozowski <pawel@brzoza.net>
2025-11-04 21:50:30 +0100

    Implement error handling for data fetching in GenderPage component; add CenteredContent for displaying error messages and improve user feedback during connection issues.

--- front-end/app/[gender]/page.jsx
+++ front-end/app/[gender]/page.jsx
@@ -1,24 +1,52 @@
 import { Bestsellers } from "../components/Bestsellers/Bestsellers";
+import { CenteredContent } from "../components/CenteredContent/CenteredContent";
 import { Hero } from "../components/Hero/Hero";
 import { GENDERS_MAPPING } from "../constants/mappings";
+import css from "./page.module.css";
 
 export default async function GenderPage({ params }) {
-  const BACKEND_URL = process.env.BACKEND_URL;
+  const BACKEND_URL = process.env.BACKEND_URLs;
   const gender = GENDERS_MAPPING.get((await params).gender);
 
-  const genderResponse = await fetch(`${BACKEND_URL}/${gender}`);
-  const genderProducts = await genderResponse.json();
+  try {
+    const genderResponse = await fetch(`${BACKEND_URL}/${gender}`);
+    const favouriteResponse = await fetch(`${BACKEND_URL}/favourites`);
 
-  const favouriteResponse = await fetch(`${BACKEND_URL}/favourites`);
-  const favouriteProducts = await favouriteResponse.json();
+    if (!genderResponse.ok || !favouriteResponse.ok) {
+      throw new Error(
+        `Błąd połączenia z bazą danych: ${genderResponse.status}`
+      );
+    }
 
-  return (
-    <>
-      <Hero imageUrl={genderProducts.heroImageUrl} />
-      <Bestsellers
-        bestsellersData={genderProducts.bestsellers}
-        favouritesData={favouriteProducts}
-      />
-    </>
-  );
+    const genderProducts = await genderResponse.json();
+    const favouriteProducts = await favouriteResponse.json();
+
+    return (
+      <>
+        <Hero imageUrl={genderProducts.heroImageUrl} />
+        <Bestsellers
+          bestsellersData={genderProducts.bestsellers}
+          favouritesData={favouriteProducts}
+        />
+      </>
+    );
+  } catch (error) {
+    console.error("Błąd połączenia z bazą danych:", error);
+
+    return (
+      <>
+        <CenteredContent>
+          <div className={css.container}>
+            <img
+              src="/images/error.png"
+              alt="Błąd połączenia z bazą danych"
+              title="Błąd połączenia z bazą danych"
+            />
+            <h2>Błąd połączenia z bazą danych</h2>
+            <p>Spróbuj ponownie za kilka minut</p>
+          </div>
+        </CenteredContent>
+      </>
+    );
+  }
 }
