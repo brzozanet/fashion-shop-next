1ae63b59841ca2fc6574b941bd7034f9362e6113
Paweł Brzozowski <pawel@brzoza.net>
2025-11-08 14:15:59 +0100

    Refactor NotFound component to update styling and improve structure. Replace not-found.module.css with styles from page.module.css, renaming classes for consistency and clarity.

--- front-end/app/error.jsx
+++ front-end/app/error.jsx
@@ -1,7 +1,8 @@
+"use client";
 import Link from "next/link";
 import css from "./page.module.css";
 
-export default function NotFound() {
+export default function Error({ error, reset }) {
   // INFO: not working, because not-found must be a server component
   // const router = useRouter();
 
@@ -9,7 +10,8 @@ export default function NotFound() {
     <>
       <div className={css.errorContainer}>
         <img src="/images/error.png" alt="Błąd 404" title="Błąd 404" />
-        <h2 className={css.errorTitle}>Nie ma takiej strony 🥺</h2>
+        <h2 className={css.errorTitle}>Coś poszło nie tak 🥺</h2>
+        <button onClick={() => reset()}>Spróbuj ponownie</button>
         <Link className={css.errorLink} href="/">
           Wróć do strony głównej
         </Link>
