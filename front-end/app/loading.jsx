910ad0fcbeb521196e612e3404c61771ca6b9441
Paweł Brzozowski <pawel@brzoza.net>
2025-11-12 09:31:47 +0100

    Add ThreeCircles loader

--- front-end/app/loading.jsx
+++ front-end/app/loading.jsx
@@ -1,25 +1,57 @@
+import { ThreeCircles } from "react-loader-spinner";
+
+// INFO: Używamy inline styles zamiast CSS Modules, aby zapobiec preloadowaniu CSS przez Next.js
+
+const containerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  margin: "3rem auto",
+};
+
 export default function Loading() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "400px",
-      }}
-    >
-      <div
-        style={{
-          width: "100px",
-          height: "100px",
-          border: "10px solid #f3f3f3",
-          borderTop: "10px solid #3498db",
-          borderRadius: "50%",
-          animation: "spin 1s linear infinite",
-        }}
-      ></div>
-      <h2>Ładowanie danych...</h2>
-    </div>
+    <>
+      <div style={containerStyle}>
+        <ThreeCircles
+          visible={true}
+          height="250"
+          width="250"
+          color="#4fa94d"
+          ariaLabel="three-circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+        <h2>Pobieram dane dla Ciebie...</h2>
+      </div>
+    </>
   );
 }
+
+// INFO: Next.js native loader
+// export default function Loading() {
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         justifyContent: "center",
+//         minHeight: "400px",
+//       }}
+//     >
+//       <div
+//         style={{
+//           width: "100px",
+//           height: "100px",
+//           border: "10px solid #f3f3f3",
+//           borderTop: "10px solid #3498db",
+//           borderRadius: "50%",
+//           animation: "spin 1s linear infinite",
+//         }}
+//       ></div>
+//       <h2>Ładowanie danych...</h2>
+//     </div>
+//   );
+// }
