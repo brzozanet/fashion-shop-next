038a09fff0980480ae2beb86f43b2de019815ef7
Paweł Brzozowski <pawel@brzoza.net>
2025-11-17 18:08:06 +0100

    Remove Pagination component to streamline the codebase.

--- front-end/app/components/Pagination/Pagination.tsx
+++ front-end/app/components/Pagination/Pagination.tsx
@@ -2,6 +2,8 @@ import { nanoid } from "nanoid";
 import Link from "next/link";
 import css from "./Pagination.module.css";
 
+// INFO: komponent zamockowany, wymaga dorobienia logiki
+
 export function Pagination() {
   const numberOfPagesMockup = 5;
   const pagination = Array(numberOfPagesMockup).fill(null);
