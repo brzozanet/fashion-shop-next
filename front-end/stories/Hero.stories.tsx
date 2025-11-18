d47f1600b6a6a97c1a20a2e48d99beb9bfb9f438
Paweł Brzozowski <pawel@brzoza.net>
2025-11-18 23:02:55 +0100

    Refactor TopBar story to improve documentation and remove unnecessary comments for better code clarity

---
+++ front-end/stories/Hero.stories.tsx
@@ -0,0 +1,10 @@
+import { Hero } from "@/app/components/Hero/Hero";
+
+export default {
+  component: Hero,
+  title: "Hero",
+};
+
+export const Default = {
+  args: { imageUrl: "/images/hero_summersale.jpg" },
+};
