2c1eb017bc35c71347712b07f912154730065cfd
Paweł Brzozowski <pawel@brzoza.net>
2025-11-18 16:17:20 +0100

    Remove GenderPage component to streamline the codebase.

--- front-end/app/[gender]/page.tsx
+++ front-end/app/[gender]/page.tsx
@@ -2,13 +2,23 @@ import { Bestsellers } from "../components/Bestsellers/Bestsellers";
 import { Hero } from "../components/Hero/Hero";
 import { GENDERS_MAPPING } from "../constants/mappings";
 import { normalizeImageUrl, normalizePhotos } from "../utils/imageNormalize";
-import Error from "../components/Error/Error";
+import ErrorComponent from "../components/Error/Error";
+import { Product } from "../types/product";
 
 // INFO: Wymusza renderowanie dynamiczne - Next.js nie będzie próbował pre-renderować tej strony podczas buildowania (co wymagałoby dostępu do backendu)
 export const dynamic = "force-dynamic";
 
-export default async function GenderPage({ params }) {
+type GenderPageProps = {
+  params: Promise<{ gender: string }>;
+};
+
+export default async function GenderPage({ params }: GenderPageProps) {
   const BACKEND_URL = process.env.BACKEND_URL;
+
+  if (!BACKEND_URL) {
+    return <ErrorComponent />;
+  }
+
   const gender = GENDERS_MAPPING.get((await params).gender);
 
   try {
@@ -30,10 +40,12 @@ export default async function GenderPage({ params }) {
     const genderProducts = await genderResponse.json();
     const favouriteProducts = await favouriteResponse.json();
 
-    const normalizedBestsellers = genderProducts.bestsellers.map((product) => ({
-      ...product,
-      photos: normalizePhotos(product.photos, BACKEND_URL),
-    }));
+    const normalizedBestsellers = genderProducts.bestsellers.map(
+      (product: Product) => ({
+        ...product,
+        photos: normalizePhotos(product.photos, BACKEND_URL),
+      })
+    );
 
     return (
       <>
@@ -48,6 +60,6 @@ export default async function GenderPage({ params }) {
     );
   } catch (error) {
     console.error("Błąd połączenia z bazą danych: ", error);
-    return <Error />;
+    return <ErrorComponent />;
   }
 }
