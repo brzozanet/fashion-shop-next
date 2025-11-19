0fba420dc10caa5b4950c91e9bafb89bc5fecb2a
Paweł Brzozowski <pawel@brzoza.net>
2025-11-19 08:09:37 +0100

    Refactor TopBar story to improve documentation and enhance code clarity by updating comments and structure.

---
+++ front-end/stories/PageTitle.stories.ts
@@ -0,0 +1,12 @@
+import { PageTitle } from "@/app/components/PageTitle/PageTitle";
+
+export default {
+  component: PageTitle,
+  title: "PageTitle",
+};
+
+export const Default = {
+  args: {
+    children: "Saepe sit amet dicta aut.",
+  },
+};
