8e53935aa7916809ea89db8652ab69716ee7b4bb
Paweł Brzozowski <pawel@brzoza.net>
2025-11-30 11:41:21 +0100

    Add test for changing currency from PLN to EUR using Playwright

--- tests/changeCurrencyFromPLNtoEUR.test.ts
+++ tests/changeCurrencyFromPLNtoEUR.test.ts
@@ -1,1 +1,18 @@
-// test
+import { test, expect } from "@playwright/test";
+
+test("Change currency from PLN to EUR", async ({ page }) => {
+  await page.goto("http://localhost:3001");
+  await page.goto("http://localhost:3001/kobieta");
+  // const selectedCurrency = page.getByRole("combobox");
+  await expect(page.getByRole("combobox")).toHaveValue("PLN");
+  await page.getByRole("combobox").selectOption("EUR");
+  await expect(page.getByRole("combobox")).toHaveValue("EUR");
+  await expect(
+    page.getByRole("link", { name: "Szpilki klasyczne 59 EUR" })
+  ).toBeVisible();
+  await page.reload();
+  await expect(page.getByRole("combobox")).toHaveValue("EUR");
+  await expect(
+    page.getByRole("link", { name: "Szpilki klasyczne 59 EUR" })
+  ).toBeVisible();
+});
