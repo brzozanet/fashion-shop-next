d171bb873fd0eef9edc5f549a10d020e2a770115
Paweł Brzozowski <pawel@brzoza.net>
2025-11-30 11:50:47 +0100

    Remove changeCurrencyFromPLNtoEUR test file to maintain a clean and efficient test suite.

--- tests/changeCurrencyPLNtoEUR.test.ts
+++ tests/changeCurrencyPLNtoEUR.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Change currency from PLN to EUR", async ({ page }) => {
+test("Change currency PLN to EUR", async ({ page }) => {
   await page.goto("http://localhost:3001");
   await page.goto("http://localhost:3001/kobieta");
 
