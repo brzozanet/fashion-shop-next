bf14ab630e5b442595b52d3080f3d7fd9be39ba8
Paweł Brzozowski <pawel@brzoza.net>
2025-11-30 11:44:16 +0100

    Enhance currency change test to verify visibility of updated product prices after switching from PLN to EUR

--- tests/changeCurrencyFromPLNtoEUR.test.ts
+++ tests/changeCurrencyFromPLNtoEUR.test.ts
@@ -3,14 +3,21 @@ import { test, expect } from "@playwright/test";
 test("Change currency from PLN to EUR", async ({ page }) => {
   await page.goto("http://localhost:3001");
   await page.goto("http://localhost:3001/kobieta");
-  // const selectedCurrency = page.getByRole("combobox");
+
   await expect(page.getByRole("combobox")).toHaveValue("PLN");
+  await expect(
+    page.getByRole("link", { name: "Szpilki klasyczne 249 PLN" })
+  ).toBeVisible();
+
   await page.getByRole("combobox").selectOption("EUR");
+
   await expect(page.getByRole("combobox")).toHaveValue("EUR");
   await expect(
     page.getByRole("link", { name: "Szpilki klasyczne 59 EUR" })
   ).toBeVisible();
+
   await page.reload();
+
   await expect(page.getByRole("combobox")).toHaveValue("EUR");
   await expect(
     page.getByRole("link", { name: "Szpilki klasyczne 59 EUR" })
